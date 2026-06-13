a893f11b362ba005b22d1a4f02640b0fb1c16661
istaude <staudeingmar@gmail.com>
2026-06-13 21:15:21 +0200

    reduce preamble to relevant packages

--- r_scripts_forpub/00-preamble.R
+++ r_scripts_forpub/00-preamble.R
@@ -1,55 +1,6 @@
-# Data handling & wrangling
-library(dplyr)
-library(tidyr)
-library(readr)
-library(readxl)
-library(data.table)
-library(stringr)
-library(forcats)
-library(lubridate)
-
-# Spatial data
 library(sf)
-library(osmdata)
-library(osmextract)
-library(rnaturalearth)
-library(rnaturalearthdata)
-library(stars)
-library(terra)
-
-# Visualization
+library(dplyr)
 library(ggplot2)
-library(ggforce)
-library(viridis)
-library(plotly)
-library(scales)
 library(patchwork)
-
-# Plant taxonomy / biodiversity data
-library(flora)
-library(rWCVP)
-library(rWCVPdata)
-
-# I/O and web interaction
-library(httr)
-library(jsonlite)
-library(writexl)
-library(htmlwidgets)
-
-# Font management
-library(showtext)
-font_add_google("Roboto Condensed", "roboto_condensed")
-showtext_auto()
-
-# Conflict management
-library(conflicted)
-
-conflict_prefer("select",   "dplyr")
-conflict_prefer("filter",   "dplyr")
-conflict_prefer("lag",      "dplyr")
-conflict_prefer("count",    "dplyr")
-conflict_prefer("mutate",   "dplyr")
-conflict_prefer("summarize","dplyr")
-conflict_prefer("arrange",  "dplyr")
-conflict_prefer("layout",   "plotly")
-conflict_prefer("year", "lubridate")
+library(viridisLite)
+library(grid)
